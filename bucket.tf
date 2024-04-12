8194dd3b431fde288325160a2c31990539c031b3
JohnKevin.Soriano <JohnKevin.Soriano@avaloq.com>
2024-04-12 10:35:37 +0800

    test

--- bucket.tf
+++ bucket.tf
@@ -1,13 +1,26 @@
-data "oci_objectstorage_namespace" "ns" {
-  compartment_id = var.compartment_ocid
+# data "oci_objectstorage_namespace" "ns" {
+#   compartment_id = var.compartment_ocid
+# }
+
+# resource "oci_objectstorage_bucket" "sample" {
+#   compartment_id = var.compartment_ocid
+#   namespace      = data.oci_objectstorage_namespace.ns.namespace
+#   name           = "test-terrateam-bucket-from-csa-shared-auth-sandboxs"
+#   access_type    = "NoPublicAccess"
+#   auto_tiering   = "Disabled"
+# }
+
+# variable "compartment_ocid" {}
+
+locals {
+  test = "Hello Terrateam!"
+  another = "Terrateam Test, with Update #1"
 }
 
-resource "oci_objectstorage_bucket" "sample" {
-  compartment_id = var.compartment_ocid
-  namespace      = data.oci_objectstorage_namespace.ns.namespace
-  name           = "test-terrateam-bucket-from-csa-shared-auth-sandboxs"
-  access_type    = "NoPublicAccess"
-  auto_tiering   = "Disabled"
+output "test" {
+  value = local.test
 }
 
-variable "compartment_ocid" {}
+output "another" {
+  value = local.another
+}
